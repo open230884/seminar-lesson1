d4b9055f5500ca043a1cdbf78d87a5ff84043c0f
Andrew PV <112729614+open230884@users.noreply.github.com>
2022-09-27 21:01:44 +0300

    рабочая прога c доработкой сроки

--- Program.cs
+++ Program.cs
@@ -4,4 +4,8 @@ int number = int.Parse(Console.ReadLine());
 
 int sq = number * number;
 
-int pow = Math.
+int pow = Convert.ToInt32(Math.Pow(number, 2));
+
+System.Console.WriteLine($"Квадрат числа равен: {sq}");
+
+System.Console.WriteLine($"Квадрат числа равен: {pow}");
